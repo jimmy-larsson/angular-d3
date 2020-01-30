94c476722afefe8d202ca7d8b5c465e1abb2e135
Jimmy Larsson <90.jimmy.larsson@gmail.com>
2020-01-31 03:58:31 +0900

    WIP: Force graph

--- src/app/d3/models/force-directed-graph.ts
+++ src/app/d3/models/force-directed-graph.ts
@@ -8,7 +8,7 @@ const FORCES = {
   LINKS: 1 / 50,
   COLLISION: 1,
   CHARGE: -1
-};
+}
 
 export class ForceDirectedGraph {
   public ticker: EventEmitter<d3.Simulation<Node, Link>> = new EventEmitter();
@@ -24,6 +24,21 @@ export class ForceDirectedGraph {
     this.initSimulation(options);
   }
 
+  connectNodes(source, target) {
+    let link;
+
+    if (!this.nodes[source] || !this.nodes[target]) {
+      throw new Error('One of the nodes does not exist');
+    }
+
+    link = new Link(source, target);
+    this.simulation.stop();
+    this.links.push(link);
+    this.simulation.alphaTarget(0.3).restart();
+
+    this.initLinks();
+  }
+
   initNodes() {
     if (!this.simulation) {
       throw new Error('Simulation has not yet been initialized.');
@@ -37,15 +52,15 @@ export class ForceDirectedGraph {
       throw new Error('Simulation has not yet been initialized.');
     }
 
-    this.simulation.force('links', d3.forceLink(this.links).strength(FORCES.LINKS));
+    this.simulation.force('links', d3.forceLink(this.links).id(d => d['id']).strength(FORCES.LINKS));
   }
 
   initSimulation(options) {
     if (!options || !options.width || !options.height) {
-      throw new Error('Missing options during simulation initialization.');
+      throw new Error('missing options when initializing simulation');
     }
 
-    // Creating the simulation
+    /** Creating the simulation */
     if (!this.simulation) {
       const ticker = this.ticker;
 
@@ -75,5 +90,4 @@ export class ForceDirectedGraph {
     /** Restarting the simulation internal timer */
     this.simulation.restart();
   }
-
 }
